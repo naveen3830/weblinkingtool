091c76b0df4f972ed41c750cf01ef353eb9f90ea
naveen3830 <naveenr3830@gmail.com>
2025-01-01 20:52:33 +0530

    updated code

--- app.py
+++ app.py
@@ -89,6 +89,14 @@ st.markdown(
         unsafe_allow_html=True
     )
 
+# config.py
+AUTHORIZED_USERS = {
+    "user1": "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",  # password: 'password'
+    "user2": "6cf615d5bcaac778352a8f1f3360d23f02f34ec182e259897fd6ce485d7870d4",  # password: 'password2'
+    "admin": "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",  # password: 'admin'
+    "naveen":"664895d207631fc1390d48165f2d0c67a3063007db9ebf60fe83490823e3151c",  # password:'naveen3830' 
+}
+
 def hash_password(password):
     """Hash a password using SHA-256."""
     return hashlib.sha256(password.encode()).hexdigest()
